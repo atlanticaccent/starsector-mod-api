c6e7f8335b3098beb33b13f88bc5a35793833678
Iain Laird <iain@laird.org>
2022-08-08 17:41:17 +0100

    Remove pointless routes
    Keep worker-version endpoint as a budget health check

--- starsector-mod-info/src/lib.rs
+++ starsector-mod-info/src/lib.rs
@@ -30,7 +30,6 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
   // functionality and a `RouteContext` which you can use to  and get route parameters and
   // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
   router
-    .get("/", |_, _| Response::ok("Hello from Workers!"))
     .post_async("/installed-mods", installed_mods)
     .get("/worker-version", |_, ctx| {
       let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
